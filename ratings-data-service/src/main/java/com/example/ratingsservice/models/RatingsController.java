a030954f0f193a5a92b21e888bf571d196984080
AbdelRahman Amgad <abdelrahmanamgad04@gmail.com>
2026-03-20 08:52:45 +0200

    reconfigured endpts to use new controller

--- ratings-data-service/src/main/java/com/example/ratingsservice/models/RatingsController.java
+++ ratings-data-service/src/main/java/com/example/ratingsservice/models/RatingsController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/ratingsdata")
+@RequestMapping("/ratings")
 public class RatingsController {
 
     private final RatingsService ratingsService;
